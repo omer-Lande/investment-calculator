7e1a0fd3f3a1d68722e774da6c872d28a0b78a2a
Omer Lande <omerl@log-on.com>
2025-03-13 15:20:50 +0200

    disabled 0 or negative duration

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ function App() {
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
       return {
@@ -23,7 +25,10 @@ function App() {
     <>
       <Header />
       <UserInput userInput={userInput} onChange={handleChange} />
-      <Results userInput={userInput} />
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than 0!</p>
+      )}
+      {inputIsValid && <Results userInput={userInput} />}
     </>
   );
 }
